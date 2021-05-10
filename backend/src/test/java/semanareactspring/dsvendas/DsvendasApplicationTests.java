963cf7a0c30a3f38a18613f59399260f4f27766f
zaissov <zaissov@yahoo.com>
2021-05-10 18:28:32 +0100

    Project created

---
+++ backend/src/test/java/semanareactspring/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package semanareactspring.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
