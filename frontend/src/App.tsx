e0bf2680a25a092d4d439f2c428759fa072e4ad8
zaissov <zaissov@yahoo.com>
2021-05-11 12:15:59 +0100

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 
+import DataTable from "components/DataTable"
+import Footer from "components/Footer"
+import NavBar from "components/Navbar"
 
 function App() {
   return (
-   <div>
-
+    <>
+    <NavBar/>
+   <div className="container">
      <h1 className="text-primary">ola Mundo</h1>
-
+     <DataTable/>
    </div>
+   <Footer/>
+   </>
   );
 }
 
