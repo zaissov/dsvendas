56c4effd181d9a18ad6313dcdbc8eff079e93e0f
zaissov <zaissov@yahoo.com>
2021-05-11 13:47:18 +0100

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,35 @@
 
+import BarChart from "components/BarChart"
 import DataTable from "components/DataTable"
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer"
 import NavBar from "components/Navbar"
 
 function App() {
   return (
     <>
-    <NavBar/>
-   <div className="container">
-     <h1 className="text-primary">ola Mundo</h1>
-     <DataTable/>
-   </div>
-   <Footer/>
-   </>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className= "text-center text-secondary">Taxa de Sucesso(%)</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5  className= "text-center text-secondary">Todas as Vendas</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+        <div className="py-3">
+        <h1 className="text-primary">Todas Vendas</h1>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,8 +4,7 @@ const Footer = () => {
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
         <p className="text-light">App desenvolvido por <a href="www.linkedin.com/in/zaissov-adao-aa7376126" target="_blank" rel="noreferrer">Zaissov Mendes</a></p>
-        <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br /></small> </p>
       </div>
     </footer>
 
